998184ea333c59a33b3993e378eacf89a12df4ed
parassareen4 <kyakrnahaibro@gmail.com>
2025-01-24 06:06:05 -0500

    heyo s

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,4 +6,28 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+
+app.post('/signup',async(c)=>{
+
+})
+
+app.post('/signin',async(c)=>{
+  
+})
+
+app.post('/blog',async(c)=>{
+  
+})
+
+app.put('/blog',async(c)=>{
+  
+})
+app.get('/blog/:id',async(c)=>{
+  
+})
+
+app.get('/blog/bulk',async(c)=>{
+  
+})
+
 export default app
