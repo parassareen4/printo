dd67283f990a785930f1ed0406759ce96cada640
parassareen4 <kyakrnahaibro@gmail.com>
2025-01-25 22:37:47 -0500

    hmm

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,26 +8,26 @@ app.get('/', (c) => {
 
 
 app.post('/signup',async(c)=>{
-
+  return c.json({message:'signup'})
 })
 
 app.post('/signin',async(c)=>{
-  
+  return c.json({message:'signin'})
 })
 
 app.post('/blog',async(c)=>{
-  
+  return c.json({message:'blog'})
 })
 
 app.put('/blog',async(c)=>{
-  
+  return c.json({message:'blog'})
 })
 app.get('/blog/:id',async(c)=>{
-  
+  return c.json({message:'blog'})
 })
 
 app.get('/blog/bulk',async(c)=>{
-  
+  return c.json({message:'blog'})
 })
 
 export default app
